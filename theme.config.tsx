3d295bbb337baac97731eb19801b176c91c441da
nabby27 <ivancordoba77@gmail.com>
2024-01-13 12:42:42 +0100

    finish getting started

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,6 +2,9 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
+  search: {
+    placeholder: 'Search',
+  },
   logo: <span>Opire</span>,
   // project: {
   //   link: 'https://github.com/shuding/nextra-docs-template',
