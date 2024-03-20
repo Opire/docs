cf3914cb1c3fc5cddfe08dac40a352564533b462
nabby27 <ivancordoba77@gmail.com>
2024-03-20 16:47:54 +0100

    Fix vercel analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,18 +1,15 @@
 import Document, { Html, Head, Main, NextScript, DocumentContext, DocumentInitialProps } from "next/document";
-import { inject } from '@vercel/analytics';
 
 class MyDocument extends Document {
     static async getInitialProps(
         ctx: DocumentContext
     ): Promise<DocumentInitialProps> {
-        const initialProps = await Document.getInitialProps(ctx)
+        const initialProps = await Document.getInitialProps(ctx);
 
-        return initialProps
+        return initialProps;
     };
 
     render() {
-        inject();
-
         return (
             <Html>
                 <Head />
